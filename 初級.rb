61398d2f4d555cb12b13af7543269b4d65b8a088
山崎慎悟 <yamazakishingo@yamazakishingonoMacBook-Pro.local>
2021-07-02 22:26:17 +0900

    chomp

--- 初級.rb
+++ 初級.rb
@@ -1,12 +1,17 @@
-age = gets.to_i
+# age = gets.to_i
 
-if age % 400 == 0
-  p "閏年です"
-elsif age % 100 == 0
-  p "閏年ではない"
-elsif age % 4 == 0
-  p "閏年です"
-else
-  p "閏年ではない"
-end
-# 条件の記述順に注意する
+# if age % 400 == 0
+#   p "閏年です"
+# elsif age % 100 == 0
+#   p "閏年ではない"
+# elsif age % 4 == 0
+#   p "閏年です"
+# else
+#   p "閏年ではない"
+# end
+# # 条件の記述順に注意する
+
+
+S = gets
+
+p S.chomp  #変数の後に改行が入る時は”chomp"で改行を削除できる
