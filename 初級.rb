5c9f24d28489d759a4a740f2a54191970e7b659a
山崎慎悟 <yamazakishingo@yamazakishingonoMacBook-Pro.local>
2021-07-07 02:44:57 +0900

    ハッシュ

--- 初級.rb
+++ 初級.rb
@@ -17,10 +17,18 @@
 # p S.chomp  #変数の後に改行が入る時は”chomp"で改行を削除できる
 
 
-m = gets.to_i
-p = gets.to_i
-q = gets.to_i
+# m = gets.to_i
+# p = gets.to_i
+# q = gets.to_i
 
-s = m - m * p.to_f/100
-a = s - s * (q.to_f/100)
-puts a.ceil(2)
+# s = m - m * p.to_f/100
+# a = s - s * (q.to_f/100)
+# puts a.ceil(2)
+
+x = {"ruby"=>1 , "php"=>2 , "java"=>3}
+p x.values # 1 2 3が表示される
+p x.keys   # ruby php javaが表示される
+x.store("python",4) #ハッシュにkeyとvalueを追加
+p x
+p x.select {|k, v| v >= 3} #３以上の値を表示
+
