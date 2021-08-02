d71024a3cacc168a52ca85d55286d1e6cfa062aa
山崎慎悟 <yamazakishingo@yamazakishingonoMacBook-Pro.local>
2021-08-02 20:19:17 +0900

    練習

---
+++ aiueo.java
@@ -0,0 +1,19 @@
+//標準入力とループ処理
+// import java.util.*;
+
+// public class Main{
+//   public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+//     String data = sc.next();
+//     System.out.println("hello" + data);
+//     data = sc.next();
+//     System.out.println("hello" + data);
+//   } 
+// }
+
+public class Main{
+  public static void main(String[] args){
+    String data = new java.util.Scanner(System.in).nextLine();
+    System.out.println(data);
+  }
+}
