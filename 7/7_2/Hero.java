676e857097ebcfa584cde77209e516fb919e135c
山崎慎悟 <yamazakishingo@yamazakishingonoMacBook-Pro.local>
2021-08-15 16:37:13 +0900

    fukusyu

--- 7/7_2/Hero.java
+++ 7/7_2/Hero.java
@@ -2,7 +2,12 @@ public class Hero{
   String name;
   int hp;
 
+  public Hero(String name){
+    this.hp = 100;
+    this.name = name;
+  }
   public Hero(){
     this.hp = 100;
+    this.name = "ダミー";
   }
 }
--- 7/7_2/Main.java
+++ 7/7_2/Main.java
@@ -1,7 +1,12 @@
 public class Main{
   public static void main(String[] args){
-    Hero h = new Hero();
-    h.name = "ウパ";
+    Hero h = new Hero("うぱ");
     System.out.println(h.name + "のHPは" + h.hp + "です");
+
+    Hero h1 = new Hero();
+    System.out.println(h1.name);
+
+    Thief t = new Thief("upa");
+    System.out.println("名前" + t.name + "HP" + t.hp + "MP" + t.mp);
   }
 }
