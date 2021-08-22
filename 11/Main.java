3a560f0549256e58d3d3d0c5ebfc68d4248a875a
山崎慎悟 <yamazakishingo@yamazakishingonoMacBook-Pro.local>
2021-08-22 17:28:00 +0900

    選択肢

--- 11/Main.java
+++ 11/Main.java
@@ -3,6 +3,18 @@ public class Main{
     Dancer d = new Dancer();
     Matango m = new Matango('A');
     d.name = "upa";
-    d.attack(m);
+    System.out.println("1~4の数字を入力してください");
+    int i = new java.util.Scanner(System.in).nextInt();
+
+    if(i == 1){
+      d.attack(m);
+    }else if(i == 2){
+      d.dance();
+    }else if(i == 3){
+      System.out.println(d.name + "は戸惑っている");
+    }else{
+      System.out.println("眠たい");
+    }
+
   }
 }
